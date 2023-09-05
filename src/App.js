185273811e24d5a3ac28f626156c89be81639877
KHarrisDev <kharrisjr.dev@gmail.com>
2023-09-05 18:20:27 -0400

    Date Counter
    useState is used to increment and decrement a count variable while increments can be increased through another state.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DateCounter />
     </div>
   );
 }
 
-export default App;
+const DateCounter = () => {
+  // set the state
+  // show state on the screen (use in the JSX)
+  // use function to update the state
+  const [count, setCount] = useState(0);
+  const [ step, setStep ] = useState(1);
+
+  const date = new Date("june 12 2027");
+  date.setDate(date.getDate() + count);
+  // setDate set the number date in a month
+  // getDate gets the date number in the current date
+
+  // decrease counter
+  const decrementStep = () => {
+    setStep((prevStep) => prevStep - 1);
+  }
+
+  // increase counter
+  const incrementStep = () => {
+    setStep((prevStep) => prevStep + 1);
+  }
+  // decrease counter
+  const decrement = () => {
+    setCount((prevCount) => prevCount - step);
+  }
+
+  // increase counter
+  const increment = () => {
+    setCount((prevCount) => prevCount + step);
+  }
+
+  return (
+    <div>
+      <button onClick={decrementStep}>-</button>
+      <span>Step: {step}</span>
+      <button onClick={incrementStep}>+</button>
+
+      <button onClick={decrement}>-</button>
+      <span>Count: {count}</span>
+      <button onClick={increment}>+</button>
+
+      <p>
+        <span>
+          {
+            count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was`
+          }
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+    </div>
+
+  );
+}
